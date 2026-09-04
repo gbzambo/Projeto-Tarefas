e0401fffd1c97fbfabd5d0b35b20f928a08964dc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-04 09:42:03 -0300

    style: iniciando estilização de tarefas

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -26,7 +26,7 @@ const Tarefas = () => {
             text: campo,
         };
         setTarefas([...tarefas, novaTarefa]);
-        setCampo();
+        setCampo("");
     }
 
     const removerTarefa = (id) => {
@@ -35,7 +35,7 @@ const Tarefas = () => {
     };
     return (
         <>
-            <div className="todo-container">
+            <div className="max-w-md mx-auto p-6 bg-amber-300 rounded-2xl border border-blue-700">
                 <h2>Minha Lista de Tarefas</h2>
                 {/* chama afunção AdicionarTarefa */}
                 <form onSubmit={adicionarTarefa} className="todo-form">
